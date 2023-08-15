12d5ae1bf23ad155221edd6e7545cc42eb0545df
ronaldoqj <ronaldoqj@gmail.com>
2023-08-15 18:30:23 +0100

    Section Services

--- src/pages/home/SectionServices.vue
+++ src/pages/home/SectionServices.vue
@@ -1,48 +1,105 @@
 <script setup lang="ts">
 import TitleDefault from 'components/interface/TitleDefault.vue'
-import { ref } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
+import { arrayChunk } from 'src/helpers/helpers'
+
+interface ICarouselItem {
+  id: number
+  image: string
+}
 
 const slide = ref(1)
+const state = reactive({
+  carousel: {
+    data: [] as Array<ICarouselItem>,
+    carouselData: [] as Array<ICarouselItem>[]
+  }
+})
+
+const setStoreDatas = (carouselData: Array<ICarouselItem>) : void => {
+  state.carousel.carouselData = arrayChunk(carouselData, 3) as unknown as Array<ICarouselItem>[]
+  console.log('sectionServices carouselData', JSON.parse(JSON.stringify(state.carousel.carouselData)))
+}
+
+const getData = (): void => {
+  const limitImages = 7
+  let countImages = 0
+  const newData:ICarouselItem[] = []
+
+  for (let index = 1; index < 16; index++) {
+    if (countImages === limitImages) {
+      countImages = 0
+    }
+    countImages += 1
+    const item: ICarouselItem = {
+      id: index,
+      image: `assets/image/tests/test-${countImages}.jpg`
+    }
+
+    newData.push(item)
+  }
+
+  state.carousel.data = newData
+  console.log(state.carousel.data)
+  setStoreDatas(state.carousel.data)
+}
+
+onMounted(() => {
+  getData()
+})
 </script>
 
 <template>
   <div class="section__default">
-    <TitleDefault title="Vídeos" color="tertiary" />
-    <div class="section__videos--carousel">
+    <TitleDefault title="Serviços" color="primary" />
+    <div v-if="!state.carousel.carouselData.length">Loading</div>
+    <div v-else class="section__services--carousel">
       <q-carousel
-        class="carousel"
-        control-color="tertiary"
+        class="carousel-section"
+        control-color="accent"
         swipeable
         animated
         v-model="slide"
-        :autoplay="200000"
+        :autoplay="700000"
         navigation
         infinite
       >
-      <q-carousel-slide :name="1" class="carousel--slide column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/mountains.jpg" />
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide v-for="(arrayItem, key) in (state.carousel.carouselData as Array<ICarouselItem>[])" :key="key" :name="key" class="carousel--slide column no-wrap">
+        <div class="row fit justify-start q-gutter-sm no-wrap">
+          <div class="container-content">
+            <q-img class="rounded-borders content-item" v-for="(item, key2) in arrayItem" :key="key2" :src="item.image" />
+            <!-- <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/quasar.jpg" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
+          </div>
         </div>
       </q-carousel-slide>
-      <q-carousel-slide :name="2" class="carousel--slide column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/parallax2.jpg" />
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <!-- <q-carousel-slide :name="2" class="carousel--slide column no-wrap">
+        <div class="row fit justify-start q-gutter-sm no-wrap">
+          <div class="container-content">
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/mountains.jpg" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/quasar.jpg" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/quasar.jpg" />
+          </div>
         </div>
       </q-carousel-slide>
       <q-carousel-slide :name="3" class="carousel--slide column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/cat.jpg" />
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+        <div class="row fit justify-start q-gutter-sm no-wrap">
+          <div class="container-content">
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/cat.jpg" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+          </div>
         </div>
       </q-carousel-slide>
       <q-carousel-slide :name="4" class="carousel--slide column no-wrap">
-        <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/material.png" />
-          <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/donuts.png" />
+        <div class="row fit justify-start q-gutter-sm no-wrap">
+          <div class="container-content">
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/material.png" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/donuts.png" />
+            <q-img class="rounded-borders content-item" src="https://cdn.quasar.dev/img/donuts.png" />
+          </div>
         </div>
-      </q-carousel-slide>
+      </q-carousel-slide> -->
       </q-carousel>
     </div>
   </div>
@@ -50,26 +107,66 @@ const slide = ref(1)
 
 <style lang="scss">
 $border-radius-control-banner: 20px;
-$banner-height: 630px;
+$height-item: 360px;
 
-.section__videos--carousel
+.section__services--carousel
 {
-  .carousel
+  .carousel-section
   {
     background-color: transparent;
+    height: inherit;
 
-    .carousel--slide  {
-      padding: 0 5px 30px 5px;
-      div {
-        div {
-          border-radius: 20px;
+    .carousel--slide
+    {
+      padding: 10px 0 50px 0;
+
+      .container-content
+      {
+        display: flex;
+        flex-direction: column;
+        width: inherit;
+        height: inherit;
+
+        .rounded-borders {
+          border-radius: $border-radius-control-banner;
+        }
+
+        .content-item {
+          margin: 5px;
+          height: $height-item;
+          cursor: pointer;
+        }
+
+        @media only screen and (min-width: $breakpoint-md)
+        {
+          flex-direction: row;
         }
       }
+      // div
+      // {
+      //   div {
+      //     border-radius: 20px;
+      //   }
+
+      //   @media only screen and (max-width: $breakpoint-md)
+      //   {
+      //     flex-direction: column;
+      //     height: inherit;
+
+      //     div {
+      //       max-height: 340px;
+      //     }
+      //   }
+      // }
     }
 
     .q-carousel__control {
       bottom: -5px;
     }
+
+    // @media only screen and (max-width: $breakpoint-md) {
+    //   height: inherit;
+    // }
   }
 }
 </style>
