3c1a4b8dc6e45af815ba2db492ab9fab18abd144
ronaldoqj <ronaldoqj@gmail.com>
2023-08-24 01:22:52 +0100

    Page-Articles

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,8 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/home/IndexPage.vue') },
-      { path: 'news', component: () => import('pages/news/IndexPage.vue') }
+      { path: 'news', component: () => import('pages/news/IndexPage.vue') },
+      { path: 'article', component: () => import('pages/article/IndexPage.vue') }
     ]
   },
 
