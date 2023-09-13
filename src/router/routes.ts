ce570df74740b0b88c969d7fc77fc9309cbb3c8a
ronaldoqj <ronaldoqj@gmail.com>
2023-09-13 18:31:19 +0100

    apresentation-adjustments | v1.0.0 - comment unused routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -36,15 +36,15 @@ const routes: RouteRecordRaw[] = [
         component: () => import('pages/departments/IndexPage.vue'),
         children: [
           { path: '', name: 'departments', component: () => import('pages/departments/DepartmentsPage.vue') },
-          { path: 'juridico', name: 'departmentsLegal', component: () => import('pages/departments/LegalPage.vue') },
-          { path: 'saude-e-condicoes-de-trabalho', name: 'departmentsHealthAndWorkingConditions', component: () => import('pages/departments/HealthAndWorkingConditionsPage.vue') },
-          { path: 'juventude-e-genero', name: 'departmentsYouthAndGender', component: () => import('pages/departments/YouthGenderPage.vue') },
-          { path: 'diversidade-e-combate-ao-racismo', name: 'departmentsDiversityAndCombatingRacism', component: () => import('pages/departments/DiversityAndCombatingRacismPage.vue') },
-          { path: 'esporte-e-lazer', name: 'departmentsSportsAndLeisure', component: () => import('pages/departments/SportsAndLeisurePage.vue') },
-          { path: 'cultura-e-sustentabilidade', name: 'departmentsCultureAndSustainability', component: () => import('pages/departments/CultureAndSustainabilityPage.vue') },
-          { path: 'aposentados-e-seguridade-social', name: 'departmentsRetireesAndSocialSecurity', component: () => import('pages/departments/RetireesAndSocialSecurityPage.vue') },
-          { path: 'formacao', name: 'departmentsFormation', component: () => import('pages/departments/FormationPage.vue') },
-          { path: 'financeiras-e-terceirizados-do-ramo-financeiro', name: 'departmentsFinancialInstitutionsAndOutsourcedFinancialSector', component: () => import('pages/departments/FinancialInstitutionsAndOutsourcedFinancialSectorPage.vue') }
+          { path: 'juridico', name: 'departmentsLegal', component: () => import('pages/departments/LegalPage.vue') }
+          // { path: 'saude-e-condicoes-de-trabalho', name: 'departmentsHealthAndWorkingConditions', component: () => import('pages/departments/HealthAndWorkingConditionsPage.vue') },
+          // { path: 'juventude-e-genero', name: 'departmentsYouthAndGender', component: () => import('pages/departments/YouthGenderPage.vue') },
+          // { path: 'diversidade-e-combate-ao-racismo', name: 'departmentsDiversityAndCombatingRacism', component: () => import('pages/departments/DiversityAndCombatingRacismPage.vue') },
+          // { path: 'esporte-e-lazer', name: 'departmentsSportsAndLeisure', component: () => import('pages/departments/SportsAndLeisurePage.vue') },
+          // { path: 'cultura-e-sustentabilidade', name: 'departmentsCultureAndSustainability', component: () => import('pages/departments/CultureAndSustainabilityPage.vue') },
+          // { path: 'aposentados-e-seguridade-social', name: 'departmentsRetireesAndSocialSecurity', component: () => import('pages/departments/RetireesAndSocialSecurityPage.vue') },
+          // { path: 'formacao', name: 'departmentsFormation', component: () => import('pages/departments/FormationPage.vue') },
+          // { path: 'financeiras-e-terceirizados-do-ramo-financeiro', name: 'departmentsFinancialInstitutionsAndOutsourcedFinancialSector', component: () => import('pages/departments/FinancialInstitutionsAndOutsourcedFinancialSectorPage.vue') }
         ]
       },
       {
